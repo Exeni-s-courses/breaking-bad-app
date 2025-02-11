139aff26cacc45e838918dd5387352b23ed72323
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-02-11 15:48:52 -0400

    first commit

---
+++ src/breakingbad/breaking-bad-app.js
@@ -0,0 +1,7 @@
+/**
+ * @param { HTMLDivElement } element
+ */
+export const BreakingBadApp = (element) => {
+    document.querySelector('#app-title').innerHTML = 'Breaking Bad App';
+    element.innerHTML = 'Loading...';
+}
