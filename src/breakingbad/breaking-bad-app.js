eaf243fd5f48906355bfe0c66dca6047308ab07a
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-02-11 21:16:51 -0400

    fetch api

--- src/breakingbad/breaking-bad-app.js
+++ src/breakingbad/breaking-bad-app.js
@@ -1,7 +1,24 @@
 /**
+ * @returns {Promise<Object>} quote information
+ */
+const fetchQuote = async () => {
+    const res = await fetch(`https://api.breakingbadquotes.xyz/v1/quotes`);
+    const data = await res.json();
+    return data[0];
+}
+
+
+/**
  * @param { HTMLDivElement } element
  */
-export const BreakingBadApp = (element) => {
+export const BreakingBadApp = async (element) => {
     document.querySelector('#app-title').innerHTML = 'Breaking Bad App';
     element.innerHTML = 'Loading...';
+    try {
+        const { quote } = await fetchQuote();
+        element.innerHTML = `${quote}`;
+    } catch (error) {
+        element.innerHTML = error;
+
+    }
 }
